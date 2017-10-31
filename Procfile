1e555232489b19f00bbfaaebb22fcfd1d3ecfb40
Unknown <simba3447@gmail.com>
2017-10-31 15:51:18 +0900

    Updated deploy setting

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
-web: python manage.py runserver
+web: gunicorn your_wsgi.wsgi --log-file -
+web: python manage.py runserver 0.0.0.0:5000 --settings=jaram_wiki.settings.deploy
--- jaram_wiki/settings/deploy.py
+++ jaram_wiki/settings/deploy.py
@@ -5,7 +5,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'jaram_wiki',
         'USER': 'admin',
-        'PASSWORD': SECRET_KEY,
+        'PASSWORD': 'Jaram@2017Admin',
         'HOST': 'localhost',
         'PORT': ''
     }
