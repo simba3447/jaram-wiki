7c4578ed451c12df5c250108e950ab1b3cfb7f5b
Unknown <simba3447@gmail.com>
2017-11-01 00:10:55 +0900

    Updated Deploy Settings

--- Procfile
+++ Procfile
@@ -1,2 +1,1 @@
 web: gunicorn your_wsgi.wsgi --log-file -
-web: python manage.py runserver --settings=jaram_wiki.settings.deploy
--- jaram_wiki/settings/deploy.py
+++ jaram_wiki/settings/deploy.py
@@ -11,4 +11,6 @@ import dj_database_url
 #         'PORT': ''
 #     }
 # }
-DATABASES['default'] =  dj_database_url.config()
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
