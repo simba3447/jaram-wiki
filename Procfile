6ff9743c55b108d0e3521a3fe7624b9e2d67bc35
Unknown <simba3447@gmail.com>
2017-10-31 23:44:52 +0900

    Updated Deploy Settings

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
 web: gunicorn your_wsgi.wsgi --log-file -
-web: python manage.py runserver 0.0.0.0:5000 --settings=jaram_wiki.settings.deploy
+web: python manage.py runserver --settings=jaram_wiki.settings.deploy
--- jaram_wiki/settings/deploy.py
+++ jaram_wiki/settings/deploy.py
@@ -1,12 +1,14 @@
 from jaram_wiki.settings.base import *
+import dj_database_url
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'jaram_wiki',
-        'USER': 'admin',
-        'PASSWORD': 'Jaram@2017Admin',
-        'HOST': 'localhost',
-        'PORT': ''
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'jaram_wiki',
+#         'USER': 'admin',
+#         'PASSWORD': 'Jaram@2017Admin',
+#         'HOST': 'localhost',
+#         'PORT': ''
+#     }
+# }
+DATABASES['default'] =  dj_database_url.config()
